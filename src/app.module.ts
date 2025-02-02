a5afb834cc674db97fa9aeedf7d94739818e4a71
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-02-01 20:05:51 -0500

    Agregar módulo de Club y refactorizar autenticación para usar LoginDto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminModule } from './modules/admin/admin.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ClubModule } from './modules/club/club.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AdminModule, AuthModule],
+  imports: [ConfigModule.forRoot(), AdminModule, AuthModule, ClubModule],
   controllers: [],
   providers: [],
 })
