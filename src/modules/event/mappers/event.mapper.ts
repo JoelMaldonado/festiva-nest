fbf01845853ec87df12cd7d29314c4677096030b
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-08-21 15:23:49 -0500

    fix: corregir la asignación de eventDatetime en el mapeo de eventos

--- src/modules/event/mappers/event.mapper.ts
+++ src/modules/event/mappers/event.mapper.ts
@@ -13,7 +13,7 @@ export function mapEvent(item: EventEntity) {
     idEventCategory: item.eventCategory?.id || null,
     nameEventCategory: item.eventCategory?.title || null,
     idStatus: item.status?.id || null,
-    eventDatetime: format(now, 'yyyy-MM-dd'),
+    eventDatetime: item.schedule?.event_date,
     eventDate: item.schedule?.event_date || null,
     startTime: item.schedule?.start_time || null,
     endTime: item.schedule?.end_time || null,
